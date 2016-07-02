ebc53cf0f1b1c6bd13ba5e08b2906d096babd6f7
Jannon Frank <jannon@cs.stanford.edu>
2016-07-01 22:27:48 -0700

    or maybe we do

---
+++ Source/Sync.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for PiedPiperKit.
+FOUNDATION_EXPORT double SyncVersionNumber;
+
+//! Project version string for PiedPiperKit.
+FOUNDATION_EXPORT const unsigned char SyncVersionString[];
+
